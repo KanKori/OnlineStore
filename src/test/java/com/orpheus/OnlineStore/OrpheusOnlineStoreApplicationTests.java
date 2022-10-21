115129eb3dd02899187dcc2f54eda76a85b262e6
KanKori <Lanokapols73>
2022-10-21 23:50:51 +0300

    init

---
+++ src/test/java/com/orpheus/OnlineStore/OrpheusOnlineStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orpheus.OnlineStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrpheusOnlineStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
