cae9a742033329e90eb9e0bb399776027e994341
KanKori <inavoinavo@gmail.com>
2022-10-29 00:04:27 +0300

    Controllers

---
+++ src/main/java/com/orpheus/OnlineStore/controller/PurchaseController.java
@@ -0,0 +1,29 @@
+package com.orpheus.OnlineStore.controller;
+
+import com.orpheus.OnlineStore.controller.dto.FinishPurchaseRequest;
+import com.orpheus.OnlineStore.controller.dto.FinishPurchaseResponse;
+import com.orpheus.OnlineStore.service.PurchaseService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@Slf4j
+@AllArgsConstructor
+public class PurchaseController {
+
+    private final PurchaseService purchaseService;
+
+    @PostMapping("/finishPurchase")
+    public ResponseEntity finishPurchase(@Valid @RequestBody FinishPurchaseRequest request) {
+        log.info("handling finish purchase request: {}", request);
+        String orderId = purchaseService.finishPurchase(request);
+        return ResponseEntity.ok(new FinishPurchaseResponse(orderId));
+    }
+
+}
